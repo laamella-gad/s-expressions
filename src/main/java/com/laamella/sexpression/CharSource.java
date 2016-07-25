fff65a681f04c1c9fcea6d1e3a78ad6679ade965
matozoid <hexagonaal@gmail.com>
2016-07-25 16:06:58 +0200

    Clean up CharSink interface

--- src/main/java/com/laamella/sexpression/CharSource.java
+++ src/main/java/com/laamella/sexpression/CharSource.java
@@ -1,21 +1,23 @@
 package com.laamella.sexpression;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 
 public class CharSource {
-    public static void push(String string, CharSink charSink) throws Exception {
-        push(new StringReader(string), charSink);
-    }
+	public static <T extends CharSink & Closeable> void push(String string, T charSink) throws Exception {
+		push(new StringReader(string), charSink);
+	}
 
-    public static void push(Reader reader, CharSink charSink) throws Exception {
-        while (true) {
-            int c = reader.read();
-            if (c == -1) {
-                charSink.close();
-                return;
-            }
-            charSink.accept((char) c);
-        }
-    }
+	public static <T extends CharSink & Closeable> void push(Reader reader, T charSink) throws IOException {
+		while (true) {
+			int c = reader.read();
+			if (c == -1) {
+				charSink.close();
+				return;
+			}
+			charSink.accept((char) c);
+		}
+	}
 }
