81c9b50b0367a66151c48d461c7f2813a55c16c5
matozoid <hexagonaal@gmail.com>
2016-07-30 15:52:47 +0200

    Add some default behaviour to Visitor.Adapter

--- src/main/java/com/laamella/sexpression/visitor/Visitor.java
+++ src/main/java/com/laamella/sexpression/visitor/Visitor.java
@@ -3,47 +3,53 @@ package com.laamella.sexpression.visitor;
 import com.laamella.sexpression.model.*;
 
 public interface Visitor<A, R> {
-	R accept(Atom atom, A arg) throws Exception;
+    R accept(Atom atom, A arg) throws Exception;
 
-	R accept(AtomList atomList, A arg) throws Exception;
+    R accept(AtomList atomList, A arg) throws Exception;
 
-	R accept(Document document, A arg) throws Exception;
+    R accept(Document document, A arg) throws Exception;
 
-	R accept(Comment comment, A arg) throws Exception;
+    R accept(Comment comment, A arg) throws Exception;
 
-	R accept(Whitespace whitespace, A arg) throws Exception;
+    R accept(Whitespace whitespace, A arg) throws Exception;
 
-	R accept(EndOfLine endOfLine, A arg) throws Exception;
+    R accept(EndOfLine endOfLine, A arg) throws Exception;
 
-	class Adapter<A, R> implements Visitor<A, R> {
-		@Override
-		public R accept(Atom atom, A arg) throws Exception {
-			return null;
-		}
+    class Adapter<A, R> implements Visitor<A, R> {
+        @Override
+        public R accept(Atom atom, A arg) throws Exception {
+            return null;
+        }
 
-		@Override
-		public R accept(AtomList atomList, A arg) throws Exception {
-			return null;
-		}
+        @Override
+        public R accept(AtomList atomList, A arg) throws Exception {
+            for (Node n : atomList.nodes()) {
+                n.visit(this, arg);
+            }
+            return null;
+        }
 
-		@Override
-		public R accept(Document document, A arg) throws Exception {
-			return null;
-		}
+        @Override
+        public R accept(Document document, A arg) throws Exception {
+            for (Node n : document.nodes()) {
+                n.visit(this, arg);
+            }
+            return null;
+        }
 
-		@Override
-		public R accept(Comment comment, A arg) throws Exception {
-			return null;
-		}
+        @Override
+        public R accept(Comment comment, A arg) throws Exception {
+            return null;
+        }
 
-		@Override
-		public R accept(Whitespace whitespace, A arg) throws Exception {
-			return null;
-		}
+        @Override
+        public R accept(Whitespace whitespace, A arg) throws Exception {
+            return null;
+        }
 
-		@Override
-		public R accept(EndOfLine endOfLine, A arg) throws Exception {
-			return null;
-		}
-	}
+        @Override
+        public R accept(EndOfLine endOfLine, A arg) throws Exception {
+            return null;
+        }
+    }
 }
